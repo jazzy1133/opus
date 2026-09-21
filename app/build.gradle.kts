188b94c2ffccf111b4f3ad95acec5393400a74e9
jazzy1133 <jagdeep2536@proton.me>
2026-09-21 12:39:49 -0400

    Rename Opus -> Outro (1.0.8)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.opus.music"
         minSdk = 26
         targetSdk = 34
-        versionCode = 8
-        versionName = "1.0.7"
+        versionCode = 9
+        versionName = "1.0.8"
         vectorDrawables {
             useSupportLibrary = true
         }
